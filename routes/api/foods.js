3604130368fb23079e8904ce1842e8a90f49e520
Denys Borysiuk <denisborisiuk@gmail.com>
2023-06-04 20:37:59 +0300

    fix

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../../controllers/order");
+const ctrl = require("../../controllers/food");
 
 router.get("/", ctrl.getAll);
 
